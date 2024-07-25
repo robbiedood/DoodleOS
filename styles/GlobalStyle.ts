95e884457314646ce77580182a34ec4b36ec1f22
JingSyue <acs108801@gm.ntcu.edu.tw>
2024-07-25 18:25:47 +0800

    add sonarqube

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   *::after {
     border: 0;
     box-sizing: border-box;
-    cursor: default;
+    cursor: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24'%3E%3Cpath d='M3.1,4.46l7.21,15.92A1.17,1.17,0,0,0,12.5,20l1.26-6.23L20,12.5a1.17,1.17,0,0,0,.39-2.19L4.46,3.1A1,1,0,0,0,3.1,4.46Z' fill='white' stroke='black' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E") 8 8, auto;
     font-variant-numeric: tabular-nums;
     margin: 0;
     outline: 0;
